215341f9610f03e2d2073bd52f8ba70fc296fa69
sebidev <sebidev@hotmail.com>
2025-08-01 01:21:17 +0200

    simple shell

--- go.mod
+++ go.mod
@@ -3,11 +3,7 @@ module netshell
 go 1.24.5
 
 require (
-	github.com/c-bata/go-prompt v0.2.6 // indirect
-	github.com/mattn/go-colorable v0.1.7 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/mattn/go-runewidth v0.0.9 // indirect
-	github.com/mattn/go-tty v0.0.3 // indirect
+	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/pkg/term v1.2.0-beta.2 // indirect
 	github.com/vishvananda/netlink v1.3.1 // indirect
 	github.com/vishvananda/netns v0.0.5 // indirect
--- main.go
+++ main.go
@@ -6,43 +6,102 @@ import (
 	"strings"
 	"text/tabwriter"
 
-	"github.com/c-bata/go-prompt"
+	"github.com/chzyer/readline"
 	"github.com/vishvananda/netlink"
 )
 
-func executor(in string) {
-	in = strings.TrimSpace(in)
-	switch in {
-		case "exit", "quit":
-			fmt.Println("Bye!")
-			os.Exit(0)
-
-		case "show link":
-			showLinks()
-
-		case "show address":
-			showAddresses()
-
-		default:
-			fmt.Println("Unknown command:", in)
-	}
+type CommandNode struct {
+	Children map[string]*CommandNode
+	Dynamic  func() []string
 }
 
-func completer(d prompt.Document) []prompt.Suggest {
-	return []prompt.Suggest{
-		{Text: "show link", Description: "Show Interfaces"},
-		{Text: "show address", Description: "Show IP-Adressen"},
-		{Text: "exit", Description: "Leave Shell"},
-		{Text: "quit", Description: "Leave Shell"},
+var rootCommand = &CommandNode{
+	Children: map[string]*CommandNode{
+		"show": {
+			Children: map[string]*CommandNode{
+				"link":    {},
+				"address": {},
+			},
+		},
+		"set": {
+			Children: map[string]*CommandNode{
+				"interface": {
+					Dynamic: getInterfaces,
+				},
+			},
+		},
+		"exit": {},
+		"quit": {},
+	},
+}
+
+func getInterfaces() []string {
+	links, _ := netlink.LinkList()
+	var names []string
+	for _, link := range links {
+		names = append(names, link.Attrs().Name)
 	}
+	return names
+}
+
+type completer struct{}
+
+func (c *completer) Do(line []rune, pos int) ([][]rune, int) {
+	input := string(line[:pos])
+	fields := strings.Fields(input)
+	prefix := ""
+	if len(fields) > 0 && !strings.HasSuffix(input, " ") {
+		prefix = fields[len(fields)-1]
+		fields = fields[:len(fields)-1]
+	}
+
+	suggestions := getSuggestions(fields, rootCommand)
+	var out [][]rune
+	for _, s := range suggestions {
+		if strings.HasPrefix(s, prefix) {
+			out = append(out, []rune(s))
+		}
+	}
+	return out, len(prefix)
+}
+
+func getSuggestions(path []string, node *CommandNode) []string {
+	if node == nil {
+		return nil
+	}
+	if len(path) == 0 {
+		var out []string
+		for k := range node.Children {
+			out = append(out, k)
+		}
+		if node.Dynamic != nil {
+			out = append(out, node.Dynamic()...)
+		}
+		return out
+	}
+
+	next := path[0]
+
+	// Bei Dynamic prüfen wir, ob z. B. eth0 existiert
+	if node.Dynamic != nil {
+		for _, opt := range node.Dynamic() {
+			if opt == next {
+				return getSuggestions(path[1:], &CommandNode{})
+			}
+		}
+	}
+
+	child, ok := node.Children[next]
+	if !ok {
+		return nil
+	}
+	return getSuggestions(path[1:], child)
 }
 
 func showLinks() {
 	links, _ := netlink.LinkList()
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-
 	fmt.Fprintln(w, "Name\tType\tState")
-
 	for _, link := range links {
 		attrs := link.Attrs()
 		state := "DOWN"
@@ -51,34 +110,56 @@ func showLinks() {
 		}
 		fmt.Fprintf(w, "%s\t%s\t%s\n", attrs.Name, link.Type(), state)
 	}
-
 	w.Flush()
 }
 
 func showAddresses() {
 	links, _ := netlink.LinkList()
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-
 	fmt.Fprintln(w, "Interface\tAddress")
-
 	for _, link := range links {
 		addrs, _ := netlink.AddrList(link, netlink.FAMILY_ALL)
 		for _, addr := range addrs {
 			fmt.Fprintf(w, "%s\t%s\n", link.Attrs().Name, addr.IPNet.String())
 		}
 	}
-
 	w.Flush()
 }
 
-
 func main() {
 	fmt.Println("Welcome to NetShell!")
-	p := prompt.New(
-		executor,
-		 completer,
-		 prompt.OptionPrefix("netshell> "),
-			prompt.OptionTitle("NetShell CLI"),
-	)
-	p.Run()
+
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:          "netshell> ",
+		AutoComplete:    &completer{},
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+		HistoryFile:     "/tmp/netshell.history",
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err != nil {
+			break
+		}
+		cmd := strings.TrimSpace(line)
+
+		switch cmd {
+			case "exit", "quit":
+				fmt.Println("Bye!")
+				return
+			case "show link":
+				showLinks()
+			case "show address":
+				showAddresses()
+			default:
+				if cmd != "" {
+					fmt.Println("Unknown command:", cmd)
+				}
+		}
+	}
 }
